8b2b95abe5f8d8dc5d8beff9c0746bf0d2945d2d
aritram1 <aritram1@gmail.com>
2022-03-21 22:14:07 +0530

    daytracker app added

--- src/client/modules/my/daytracker/daytracker.js
+++ src/client/modules/my/daytracker/daytracker.js
@@ -1,5 +1,53 @@
 import { LightningElement, api } from 'lwc';
-// import LoginUtil from './util.js';
+import util from './util';
+
 export default class DayTracker extends LightningElement {
    @api source; // To refer jsfiddle link if any
+   util = util();
+   fillerWorkSelectedValue = '';
+   dailyActivities = [];
+   fillerWorkTypes = [];
+   currentActivities = [];
+   currentTime = '';
+
+   connectedCallback() {
+      this.dailyActivities = this.util.generateFakeDailyActivities();
+      this.fillerWorkTypes = this.util.generateFakeFillerWorkTypes();
+      this.currentActivities = this.util.generateFakeCurrentActivities();
+      setInterval(() => {
+         let today = new Date();
+         this.currentTime = `
+            ${today.getHours().toString().padStart(2, '0')}:
+            ${today.getMinutes().toString().padStart(2, '0')}:
+            ${today.getSeconds().toString().padStart(2, '0')}`;
+      }, 1000);
+   }
+
+   handleFillerWorkChange(e) {
+      this.fillerWorkSelectedValue = e.target.value;
+      console.log('Filler work changed to :' + this.fillerWorkSelectedValue);
+   }
+
+   handleStartFillerWork() {
+      // this.currentActivities = [this.currentActivities, {
+
+      // }];
+      console.log(`${this.fillerWorkSelectedValue} started at ${new Date()}`);
+   }
+
+   handleCompleteCurrentActivity(e) {
+      console.log('Act Id : ' + e.target.dataset.actid);
+   }
+
+   handleDailyActivityComplete(e) {
+      console.log('Act Id : ' + e.target.dataset.actid);
+   }
+
+   handleScroll(e) {
+      if (e.target.classList.contains('up')) {
+         console.log('In up scroll');
+      } else {
+         console.log('In down scroll');
+      }
+   }
 }
--- src/client/modules/my/daytracker/util.js
+++ src/client/modules/my/daytracker/util.js
@@ -1,62 +1,80 @@
-// //class approach
-// export default class Util{
-//     constructor(){
-//         console.log('Util invoked at' + new Date().toLocaleTimeString()),
-//     }
-//     add(a,b){
-//         return a+b,
-//     }
-// }
-
-// function approach
-
+//let jsforce = require('jsforce');
 export default function LoginUtil() {
-   // return {
-   //     client_id :'3MVG9fe4g9fhX0E7rQAj1RID4yDleF8mdGj25.74_AQyBQQDZCnTCBehxp4hb.WzOkV4ZzvpwlFqlvn_8mbXt',
-   //     client_secret : '707130F2DB959D40D26B949C2508BE6D0F52B049679E20B615DC557BCDBE9DF2',
-   //     token_endpoint : 'https://login.salesforce.com/services/oauth2/token',
-   //     auth_endpoint : 'https://login.salesforce.com/services/oauth2/authorize',
-   //     redirect_uri : 'https://localhost',
-   //     state: {
-   //         token : '',
-   //         expiresAt : null,
-   //         instance_url : '',
-   //         authorized : false,
-   //         authCode : ''
-   //     },
-   //     //functions
-   //     async getToken(){
-   //         console.log('Inside getToken');
-   //     },
-   //     async refreshToken(){
-   //         console.log('Inside refreshToken');
-   //     },
-   //     async getAuthorized(){
-   //         console.log('Inside getAuthorized');
-   //         if(!this.authorized){
-   //             let loginData = new FormData();
-   //             loginData.append("client_id", this.client_id);
-   //             loginData.append("redirect_uri", this.redirect_uri);
-   //             loginData.append("grant_type", 'password');
-   //             loginData.append("client_Secret", this.client_secret);
-   //             loginData.append("username", 'aritram1@gmail.com');
-   //             loginData.append("password", 'Aritra1985#');
-   //             let resp = await fetch(this.token_endpoint,{
-   //                 method: 'POST',
-   //                 body : loginData
-   //             });
-   //             let respBody = await resp.json();
-   //             console.log('Authcode received as: ' + respBody.data);
-   //             //this.authCode = resp;
-   //             this.authorized = true;
-   //         }
-   //     },
-   //     getOptions(){
-   //         return [
-   //             {label : 'eat', value : 'eat'},
-   //             {label : 'drink', value : 'drink'},
-   //             {label : 'sleep', value : 'sleep'}
-   //         ];
-   //     }
-   // }
+   let uname = 'aritram1@gmail.com.iot';
+   let pwd = 'Aritra1985#';
+   return {
+      // loginresponse: {},
+      // connectToSF: function () {
+      //    let conn = new jsforce.Connection();
+      //    conn.login(uname, pwd, function (err, res) {
+      //       if (err) {
+      //          this.loginresponse = { err: err };
+      //       } else if (res) {
+      //          this.loginresponse = { res: res };
+      //       }
+      //       return this.loginresponse;
+      //    });
+      // },
+
+      ///////////////////////utility methods/////////////////
+      generateFakeDailyActivities: function () {
+         let temp = [];
+         temp.push({
+            Id: '100',
+            name: 'lunch',
+            start: '02.00PM',
+            finish: '03.00PM',
+         });
+         temp.push({
+            Id: '101',
+            name: 'snacks',
+            start: '07.00PM',
+            finish: '08.00PM',
+         });
+         temp.push({
+            Id: '102',
+            name: 'dinner',
+            start: '10.00PM',
+            finish: '11.00PM',
+         });
+         temp.push({
+            Id: '103',
+            name: 'sleep',
+            start: '11.30PM',
+            finish: '05.00AM',
+         });
+         return temp;
+      },
+      generateFakeFillerWorkTypes() {
+         let temp = [];
+         temp.push({ Id: '200', label: 'Playing game', value: 'games' });
+         temp.push({ Id: '201', label: 'Watching youtube', value: 'youtube' });
+         temp.push({ Id: '202', label: 'Other', value: 'other' });
+         return temp;
+      },
+      generateFakeCurrentActivities() {
+         let temp = [];
+         temp.push({
+            Id: '300',
+            name: 'Playing games',
+            timeElapsed: 30,
+            status: 'inprogress',
+         });
+         temp.push({
+            Id: '301',
+            name: '.......',
+            timeElapsed: 20,
+            status: 'inprogress',
+         });
+         temp.push({
+            Id: '302',
+            name: '........',
+            timeElapsed: 40,
+            status: 'inprogress',
+         });
+         return temp;
+      },
+
+      ///////////////////////utility methods/////////////////
+   };
 }
